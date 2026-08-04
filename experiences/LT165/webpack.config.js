be312f65118620340c8e51846d681884dcb55a6f
AleexCaires <tiagoaleex98@gmail.com>
2026-08-04 10:16:16 +0100

    Initial Scaffold

---
+++ experiences/LT165/webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const baseConfig = require('../../config/webpack.experience.js');
+
+module.exports = {
+  ...baseConfig('LT165'),
+  // Override base config here if needed
+};
